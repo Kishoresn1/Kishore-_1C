4a4cb11b64ad5c746dd78655285fad118dd7857c
Kishoresn1 <kishorsnagaraju13@gmail.com>
2024-11-26 14:50:36 +0530

    Create  While loop.c

---
+++ While loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;  // Initialize variable
+
+    // While loop continues as long as i is less than or equal to 5
+    while (i <= 5) {
+        printf("%d\n", i);  // Print the current value of i
+        i++;  // Increment i
+    }
+
+    return 0;
+}
